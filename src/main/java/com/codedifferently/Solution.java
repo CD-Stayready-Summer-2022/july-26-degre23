2055868b1c3d509294a770b7e131bf2c5eada576
degre23 <98403668+degre23@users.noreply.github.com>
2022-07-26 09:33:48 -0400

    july26

--- src/main/java/com/codedifferently/Solution.java
+++ src/main/java/com/codedifferently/Solution.java
@@ -1,7 +1,19 @@
 package com.codedifferently;
 
+import java.lang.reflect.Array;
+import java.util.TreeMap;
+
 public class Solution {
     public String howManyLettersDoYouSee(String input){
+        // create treemap<integer, string>
+        // split the list
+        TreeMap<Integer, String> letterList = new TreeMap<>();
+        String arr = input;
+
+        for (int i = 0; i < input.length(); i++){
+
+        }
+
         return null;
     }
 }
